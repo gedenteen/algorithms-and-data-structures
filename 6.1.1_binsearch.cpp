892f6a9490776cdcca56ea885674ebb0e28086a1
gedenteen <47585081+gedenteen@users.noreply.github.com>
2020-08-08 10:05:48 +0700

    Create 6.1.1_binsearch.cpp

---
+++ 6.1.1_binsearch.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <vector>
+
+int get_pos(const std::vector<int> &numbers, int number) {
+  // TODO optimize this function
+  int n = numbers.size(), l = 0, r = n, m, i;
+  for (i = 0; i < n; i++) {
+      while (l < r) {
+          m = (l + r) / 2;
+          if (number > numbers[m])
+              l = m + 1;
+          else
+              r = m;
+      }
+      if (number == numbers[l])
+          return l + 1;
+      else
+          return -1;
+  }
+}
+
+int main(void) {
+  size_t number_count;
+  std::cin >> number_count;
+  std::vector<int> numbers(number_count);
+  for (auto &number:numbers) {
+    std::cin >> number;
+  }
+  assert(std::is_sorted(numbers.begin(), numbers.end()));
+
+  size_t query_count;
+  std::cin >> query_count;
+  while (query_count-- > 0) {
+    int number;
+    std::cin >> number;
+    std::cout << get_pos(numbers, number) << " ";
+  }
+  std::cout << std::endl;
+}
